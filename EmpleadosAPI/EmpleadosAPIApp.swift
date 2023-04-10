aa1f378df46852870ec8e01e8396386591bc2344
Roger Espinoza <rj.espinoza44@gmail.com>
2023-04-10 13:02:14 -0500

    Initial Commit

---
+++ EmpleadosAPI/EmpleadosAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmpleadosAPIApp.swift
+//  EmpleadosAPI
+//
+//  Created by Roger Espinoza on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct EmpleadosAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
